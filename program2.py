e117e0f52e24ecf5d8625b6f597ce2609899ea3f
Riya Meena <167178471+Riyameena18@users.noreply.github.com>
2024-09-27 14:01:48 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,38 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+   class Solution(object):
+   def roman_to_int(s: str) -> int:
+    # Mapping of Roman numerals to their integer values
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    
+    for char in reversed(s):
+        current_value = roman_values[char]
+        
+        # If the current value is less than the previous value, subtract it
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value
+        
+        prev_value = current_value  # Update the previous value
+    
+    return total
+
+# Get user input
+user_input = input("Enter a Roman numeral: ")
+result = roman_to_int(user_input)
+print(f"The integer value of the Roman numeral {user_input} is {result}.")
 
 
 
